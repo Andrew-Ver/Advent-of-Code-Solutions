4f8511624b678e91910bf8614bab3c7e85aae3da
Andrew-Ver <41878065+Andrew-Ver@users.noreply.github.com>
2023-05-21 18:17:04 -0400

    Day 9

--- 2022/day-09/day-09.py
+++ 2022/day-09/day-09.py
@@ -3,44 +3,71 @@ import re
 with open('input', 'r') as f:
     puzzle_input: list[tuple[str, int]] = [(re.search(r'\w', ln).group(), int(re.search(r'\d+', ln).group())) for ln in f.readlines()]
 
-head_X = head_Y = tail_X = tail_Y = 0
+def solution(n_nodes: int, puzzle_input=puzzle_input):
+    def move_head(pos: tuple[int], direction: str) -> tuple[int, int]:
+        '''
+            New head position after one step based on direction
+        '''
+        Y, X = pos
+        movement: dict[str, tuple[int]] = {'R': (Y, X+1),
+                                           'D': (Y+1, X),
+                                           'L': (Y, X-1),
+                                           'U': (Y-1, X)}
+        return movement[direction]
 
-visited: set[int] = set()
+    def move_tail(head_position: tuple[int], tail_position: tuple[int]) -> tuple[int]:
+        diff_y: int = (head_position[0] - tail_position[0])
+        diff_x: int = (head_position[1] - tail_position[1])
+        Y, X = tail_position
 
-def difference(x_1: int, y_1: int, x_2: int, y_2: int) -> tuple[int]:
-    return (x_1-x_2, y_1-y_2)
+        # Left/right movement
+        movement: dict[str, tuple[int]] = {
+            (0, 2): (Y, X+1),
+            (0, -2): (Y, X-1),
+            (-2, 0): (Y-1, X),
+            (2, 0): (Y+1, X),
+            # For part two
+            (2, 2): (Y+1, X+1),
+            (2, -2): (Y+1, X-1),
+            (-2, 2): (Y-1, X+1),
+            (-2, -2): (Y-1, X-1),
+        }
 
-head_X, head_Y = (0, 4)
-tail_X, tail_Y = (1, 5)
+        if (diff_y, diff_x) in movement:
+            return movement[(diff_y, diff_x)]
 
-print(difference(head_X, head_Y, tail_X, tail_Y))
+        # Handle Diagonal movements
+        # ex. Difference of (2, 1) => (1, 1)
+        if diff_y == 2:
+            return (Y+1, X+diff_x)
+        elif diff_y == -2:
+            return (Y-1, X+diff_x)
+        elif diff_x == 2:
+            return (Y+diff_y, X+1)
+        elif diff_x == -2:
+            return(Y+diff_y, X-1)
+        return (tail_position)
 
-for direction, steps in puzzle_input:
-    for _ in range(steps):
-        if direction == 'U':
-            head_Y -= 1
-        elif direction == 'D':
-            head_Y += 1
-        elif direction == 'L':
-            head_X -= 1
-        elif direction == 'R':
-            head_X += 1
+    tail_visited: set[int] = set()
+    # The first one will the "head", the rest are the tails following
+    # the next node in list
+    node_positions: list[tuple[int]] = [(0, 0) for _ in range(n_nodes)]
 
-        x_diff, y_diff = difference(head_X, head_Y, tail_X, tail_Y)
+    for d, amount in puzzle_input:
+        for _ in range(amount):
+            '''
+                Move the head independent of other nodes,
+                other nodes i move in relation to node i-1
+            '''
+            for i in range(len(node_positions)):
+                if i == 0:
+                    node_positions[i] = move_head(pos=node_positions[i], direction=d)
+                else:
+                    node_positions[i] = move_tail(head_position=node_positions[i-1], tail_position=node_positions[i])
 
-        # Move up/down/left/right
-        if (not x_diff and y_diff == 2):
-            tail_Y += 1
-        elif (not x_diff and y_diff == -2):
-            tail_Y -= 1
-        elif (x_diff == 2 and not y_diff):
-            tail_X += 1
-        elif (x_diff == -2 and not y_diff):
-            tail_X -= 1
+            tail_visited.add(node_positions[-1])
 
-        # Move diagnonally
-        elif (x_diff and y_diff):
+    return len(tail_visited)
 
-
-
-            visited.add((tail_X, tail_Y))
+print(f'Part One: {solution(2)}.')
+print(f'Part Two: {solution(10)}')
